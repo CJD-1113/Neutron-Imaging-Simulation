620cf3dc4659ddcddda6361d6881ed8786d9f587
CJD-1113 <100615606+CJD-1113@users.noreply.github.com>
2023-08-07 11:34:40 -0400

    updated graph

--- Functions/XMLtoVTI.py
+++ Functions/XMLtoVTI.py
@@ -1,8 +1,17 @@
 #Creating file to be displayed in paraview
-vox_plot = openmc.Plot()
-vox_plot.type = 'voxel'
-vox_plot.width = (1500., 1500., 1500.)
-vox_plot.pixels = (200, 200, 200)
-vox_plot.color_by = 'material'
-vox_plot.colors = {castBronze: 'yellow'}#, oxygen: 'red'}  # materials can be coloured using this command
-vox_plot.to_vtk(output='cylinder.vti')
+plot = openmc.Plot()
+plot.type = 'voxel'
+plot.width = (100., 100., 60.)
+plot.pixels = (400, 400, 200)
+plot.basis = 'yz'
+plot.origin = (5.0, 2.0, 3.0)
+plot.color_by = 'material'
+plot.colors = {
+    airmix: 'blue',
+    castBronze: 'black'
+}
+
+plots = openmc.Plots()
+plots.append(plot)
+plots.export_to_xml()
+plot.to_vtk(output='cylinder.vti')
