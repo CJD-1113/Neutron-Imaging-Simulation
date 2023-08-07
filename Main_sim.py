e378983b3ba5c368d049ac10d2e831d9815eed04
JackSzyp <124404266+JackSzyp@users.noreply.github.com>
2023-08-07 10:43:01 -0400

    Update Main_sim.py

--- Main_sim.py
+++ Main_sim.py
@@ -1,12 +1,12 @@
 %matplotlib inline
 import openmc
 import os
-import math
+import numpy as np
 
 
 castBronze = openmc.Material(1, "castBronze")
-castBronze.add_element("Cu", 0.89)
-castBronze.add_element("Sn", 0.11)
+castBronze.add_element("Cu", 0.90)
+castBronze.add_element("Sn", 0.10)
 castBronze.temperature = 293 #K
 castBronze.set_density("g/cm3", 8.77)
 
@@ -33,6 +33,7 @@ CO2.temperature = 293 #K
 
 airmix = openmc.Material.mix_materials([nitrogen, oxygen, argon, CO2], [0.7808, 0.2095, 0.0093, 0.0004], 'ao')
 mats = openmc.Materials([castBronze, nitrogen, oxygen, argon, CO2, airmix])
+print(mats)
 mats.export_to_xml()
 
 
@@ -40,9 +41,13 @@ mats.export_to_xml()
 
 
 # GEOMETRY
+cylWidth = 1.01092
+cylDiam = 2.51968
+
+
 #Making cylinder
-cylinderSurface = openmc.ZCylinder(r=1.2, boundary_type='transmission')
-upperSurface = openmc.ZPlane(z0=21, boundary_type='transmission')
+cylinderSurface = openmc.ZCylinder(r=(cylDiam/2), boundary_type='transmission')
+upperSurface = openmc.ZPlane(z0=(20+cylWidth), boundary_type='transmission')
 lowerSurface = openmc.ZPlane(z0=20, boundary_type='transmission')
 
 cylinderInside = -cylinderSurface & -upperSurface & +lowerSurface
@@ -54,19 +59,37 @@ cylinder = openmc.Cell(name='cylinder')
 cylinder.fill = castBronze
 cylinder.region = cylinderInside
 
-"""
-planeFront = openmc.ZPlane(z0=0, boundary_type='vacuum')
-planeBoundary = openmc.ZPlane(z0=25, boundary_type='vacuum')
-planeTop = openmc.XPlane(x0=2, boundary_type='vacuum')
-planeBottom = openmc.XPlane(x0=-1, boundary_type='vacuum')
-planeRight = openmc.YPlane(y0=2, boundary_type='vacuum')
-planeLeft = openmc.YPlane(y0=-1, boundary_type='vacuum')
 
-air_region = -planeFront & +planeBoundary & -planeTop & +planeBottom & +planeRight & -planeLeft
-air = openmc.Cell(name='air')
-air.fill = airmix
-air.region = air_region
-"""
+BtestcylinderSurface = openmc.ZCylinder(r=(cylDiam/2), boundary_type='transmission')
+BtestupperSurface = openmc.ZPlane(z0=20, boundary_type='transmission')
+BtestlowerSurface = openmc.ZPlane(z0=19, boundary_type='transmission')
+
+BtestcylinderInside = -BtestcylinderSurface & -BtestupperSurface & +BtestlowerSurface
+
+
+
+# Create cells, mapping materials to cells
+Btestcylinder = openmc.Cell(name='Btestcylinder')
+Btestcylinder.fill = airmix
+Btestcylinder.region = BtestcylinderInside
+
+TtestcylinderSurface = openmc.ZCylinder(r=(cylDiam/2), boundary_type='transmission')
+TtestupperSurface = openmc.ZPlane(z0=(21+cylWidth), boundary_type='transmission')
+TtestlowerSurface = openmc.ZPlane(z0=(20+cylWidth), boundary_type='transmission')
+
+TtestcylinderInside = -TtestcylinderSurface & -TtestupperSurface & +TtestlowerSurface
+
+
+
+# Create cells, mapping materials to cells
+Ttestcylinder = openmc.Cell(name='Ttestcylinder')
+Ttestcylinder.fill = airmix
+Ttestcylinder.region = TtestcylinderInside
+
+
+
+# Create a geometry and export to XML
+
 xmin = openmc.XPlane(-60.0, boundary_type='vacuum')
 xmax = openmc.XPlane(60.0, boundary_type='vacuum')
 ymin = openmc.YPlane(-60.0, boundary_type='vacuum')
@@ -74,18 +97,19 @@ ymax = openmc.YPlane(60.0, boundary_type='vacuum')
 zmin = openmc.ZPlane(-60.0, boundary_type='vacuum')
 zmax = openmc.ZPlane(60.0, boundary_type='vacuum')
 box = +xmin & -xmax & +ymin & -ymax & +zmin & -zmax
-air_region = ~cylinderInside & box
+air_region = ~cylinderInside & ~BtestcylinderInside & ~TtestcylinderInside & box
 air = openmc.Cell(name='air')
 air.fill = airmix
 air.region = air_region
 
-# Create a geometry and export to XML
-root_universe = openmc.Universe(cells=[cylinder, air])
+
+root_universe = openmc.Universe(cells=[cylinder, Ttestcylinder, Btestcylinder, air])
 geom = openmc.Geometry(root_universe)                    
 geom.export_to_xml()  
 
-root_universe.plot(width=(30,30))
-root_universe.plot(width=(30,30), basis='yz')
+root_universe.plot(width=(50,50))
+root_universe.plot(width=(50,50), basis='yz')
+
 
 
 
@@ -96,15 +120,15 @@ settings.batches = 10
 settings.inactive = 0
 
 
-pi = math.pi
 r = openmc.stats.PowerLaw(0.0, 2, 1.0)
-phi = openmc.stats.Uniform(0.0, 2*pi)
+phi = openmc.stats.Uniform(0.0, 2 * np.pi)
 z = openmc.stats.Discrete([1.0], [0.0])
 spatial_dist = openmc.stats.CylindricalIndependent(r, phi, z)
 source = openmc.Source(space=spatial_dist)
 
 
 source.angle = openmc.stats.Monodirectional([0, 0, 1])
+source.energy = openmc.stats.Discrete([0.025], [1.0])
 source.strength = 1
 settings.source = source
 
@@ -112,18 +136,25 @@ settings.run_mode = 'fixed source'
 
 settings.export_to_xml()
 
-cell_filter = openmc.CellFilter([cylinder])
-# energy_filter = openmc.EnergyFilter([0.01, 0.3])
-tally = openmc.Tally(name="Neutron Flux")
-tally.filters = [cell_filter]
 
-tally.scores = ["flux"]
 
-tallies = openmc.Tallies([tally])
+
+Ttest_filter = openmc.CellFilter(Ttestcylinder)
+
+Ttest_tally = openmc.Tally(name="flux of Top test cylinder")
+Ttest_tally.filters = [Ttest_filter]
+Ttest_tally.scores = ['flux']
+
+Btest_filter = openmc.CellFilter(Btestcylinder)
+
+Btest_tally = openmc.Tally(name="flux of Bottom test cylinder")
+Btest_tally.filters = [Btest_filter]
+Btest_tally.scores = ['flux']
+
+tallies = openmc.Tallies([Ttest_tally, Btest_tally])
 tallies.export_to_xml()
 
 model=openmc.model.Model(geom, mats, settings, tallies)
 openmc.run()
-
 
 !cat tallies.out
